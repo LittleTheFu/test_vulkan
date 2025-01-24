83f1119d43b94b30887d798f9e7a2dfaf8ea51f5
LittleTheFu <lttlstpd@live.com>
2025-01-24 14:10:47 +0800

    The chapter of 'Base code' is done

--- main.cpp
+++ main.cpp
@@ -5,34 +5,63 @@
 #include <stdexcept>
 #include <cstdlib>
 
-class HelloTriangleApplication {
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication
+{
 public:
-    void run() {
+    void run()
+    {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
-    void initVulkan() {
+    GLFWwindow *window;
 
+private:
+    void initWindow()
+    {
+        glfwInit();
+        
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
 
-    void mainLoop() {
-
+    void initVulkan()
+    {
     }
 
-    void cleanup() {
+    void mainLoop()
+    {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
+    }
 
+    void cleanup()
+    {
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
 };
 
-int main() {
+int main()
+{
     HelloTriangleApplication app;
 
-    try {
+    try
+    {
         app.run();
-    } catch (const std::exception& e) {
+    }
+    catch (const std::exception &e)
+    {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
