284184af83d72d2749ef0881b456d297f10c364f
LittleTheFu <lttlstpd@live.com>
2025-01-25 16:42:04 +0800

    "Swap chain" is done

--- main.cpp
+++ main.cpp
@@ -87,6 +87,9 @@ private:
     VkQueue presentQueue;
     VkSurfaceKHR surface;
     VkSwapchainKHR swapChain;
+    std::vector<VkImage> swapChainImages;
+    VkFormat swapChainImageFormat;
+    VkExtent2D swapChainExtent;
 
 private:
     void initWindow()
@@ -159,6 +162,13 @@ private:
         {
             throw std::runtime_error("failed to create swap chain!");
         }
+
+        vkGetSwapchainImagesKHR(device, swapChain, &imageCount, nullptr);
+        swapChainImages.resize(imageCount);
+        vkGetSwapchainImagesKHR(device, swapChain, &imageCount, swapChainImages.data());
+
+        swapChainImageFormat = surfaceFormat.format;
+        swapChainExtent = extent;
     }
 
     SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device)
